89d94892c0ccbebd50bb5d3eb2e4b5e77439bea8
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2022-06-21 15:43:46 -0300

    💻 Feat: cria elementos do Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,27 @@ import React from 'react';
 function Header() {
   return (
     <header className="App-header">
-      <h1>Hello world</h1>
+      <section>
+        <h1>iBook</h1>
+
+        <form>
+          <input type="text" name="" id="" />
+          <button type="submit">
+            Buscar
+          </button>
+        </form>
+      </section>
+
+      <section>
+        <form>
+          <span>Filtrar ano da publicação</span>
+          <input type="date" name="" id="" />
+          <span>até</span>
+          <input type="date" name="" id="" />
+        </form>
+
+        <span>0 resultados encontrados</span>
+      </section>
     </header>
   );
 }
