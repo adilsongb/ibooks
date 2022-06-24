ead92368c5be986e1bb717d648fe37a1f751c5f7
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2022-06-23 23:49:50 -0300

    🎨 Feat: estiliza componente search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,6 @@
 import { useContext, useState, MouseEvent } from 'react';
 import { AppContext } from '../contexts/AppProvider';
+import searchIcon from '../images/icons/search.svg';
 
 function Search() {
   const [query, setQuery] = useState('');
@@ -11,14 +12,14 @@ function Search() {
   };
 
   return (
-    <form>
+    <form className="Search">
       <input
         type="text"
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Busque livros pelo título, autor, ou idioma"
       />
       <button type="submit" onClick={submitSearch}>
-        Buscar
+        <img src={searchIcon} alt="Search icon" />
       </button>
     </form>
   );
